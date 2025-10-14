04a757ef162cc05416fa67f3663d6985449e4fa2
zibaakord <zibaaakord@gmail.com>
2025-10-14 22:02:20 +0330

    chore: setup basic chatroom structure with known bug

---
+++ public/client.js
@@ -0,0 +1,35 @@
+const socket = io('http://localhost:5500');
+const currentUser = 'Ziba';
+
+function sendMessage() {
+  const messageInput = document.getElementById('input-message');
+  const message = messageInput.value.trim();
+  console.log('Trying to send message:', message);
+  if (message) {
+    socket.emit('chatMessage', { sender: currentUser, text: message });
+    messageInput.value = '';
+  }
+}
+
+socket.on('chatMessage', (message) => {
+  const messages = document.getElementById('messages');
+  const messageDiv = document.createElement('div');
+  
+  messageDiv.textContent = message.text || message;
+  
+  messageDiv.classList.add(
+    message.sender === currentUser ? 'own-message' : 'others-message'
+  );
+  
+  messages.appendChild(messageDiv);
+  messages.scrollTop = messages.scrollHeight;
+});
+
+
+const sendButton = document.getElementById('send-button');
+const input = document.getElementById('input-message');
+
+sendButton.addEventListener('click', sendMessage);
+input.addEventListener('keypress', e => {
+  if (e.key === 'Enter') sendMessage();
+});
